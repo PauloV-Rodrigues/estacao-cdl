74b6784f072c065c82d668cfb3e29c16fa769ebf
Paulo Rodrigues <99622137+PauloV-Rodrigues@users.noreply.github.com>
2022-03-28 12:32:30 -0300

    Hidding SECRET_KEY

--- estacaocdl/settings.py
+++ estacaocdl/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l*&sn^^s%oh0r4c10z_1^hn1jcwh2#_*q+24h@+un$sb&^ip3u'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
